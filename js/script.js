e0853b836717630c3050f4061dcef4faee55f61a
Maciej <108257743+MKarwat93@users.noreply.github.com>
2022-11-30 23:57:26 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const burgerBtn = document.querySelector(".burger-button")
+const navMenu = document.querySelector('.nav-container')
+
+burgerBtn.addEventListener("click", () => {burgerBtn.toggle(".open")})
